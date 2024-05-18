3ac340de0aa6c9bde0acd18838e5206d578d1b14
Raj-Mandal-20 <usingrajcseinfo@gmail.com>
2024-05-19 00:56:32 +0530

    changed navbar logo

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -2,6 +2,6 @@ import React from 'react';
 
 import './Logo.css';
 
-const logo = props => <h1 className="logo">Quick Chat</h1>;
+const logo = props => <h1 className="logo"> 🗨️ Quick Chat</h1>;
 
 export default logo;
